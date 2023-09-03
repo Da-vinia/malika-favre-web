5caf91e263e95bb7865bb5bda98ab60d9e67c1bf
Da-vinia <daviniatosco@gmail.com>
2023-09-03 11:02:37 +0200

    create web express

---
+++ app.js
@@ -0,0 +1,19 @@
+const exp = require("constants");
+const express = require("express");
+const app = express();
+
+app.get("/home", (request, response, next) =>
+    response.sendFile(__dirname + "/views/home.html")
+);
+
+app.get("/about", (request, response, next) =>
+    response.sendFile(__dirname + "/views/about.html")
+);
+
+app.get("/works", (request, response, next) =>
+    response.sendFile(__dirname + "/views/works.html")
+);
+
+app.use(express.static("public"));
+
+app.listen(3000, () => console.log("My second app listening on port 3000!"));
